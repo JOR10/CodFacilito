1c276a46685374eabb2e1f8daafd0f53c939db86
Jorge A. Quijano Silva <69021512+JOR10@users.noreply.github.com>
2024-09-03 18:54:50 +0000

    Reto 5

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,6 +1,6 @@
 // Placeholder to host a component
 import React, { useState, useEffect } from 'react';
-
+/*
 const HoraDelDia = () => {
   const [hora, setHora] = useState(new Date());
 
@@ -30,6 +30,34 @@ const HoraDelDia = () => {
       <h2><p> {formatHora(hora)}</p></h2>
     </div>
   );
-};
+};*/
+const HoraDelDia = () => {
+  // add logic
+
+  const shelterHours = [
+      { day: "Monday", open: "10:00", close: "16:00" },
+      { day: "Tuesday", open: "10:00", close: "16:00" },
+      { day: "Wednesday", open: "10:00", close: "16:00" },
+      { day: "Thursday", open: "10:00", close: "16:00" },
+      { day: "Friday", open: "10:00", close: "16:00" },
+      { day: "Saturday", open: "9:00", close: "20:00" },
+      { day: "Sunday", open: "9:00", close: "20:00" },
+
+  ]
+
+  // get the long day name and store it in a variable called today
+  const today = new Date().toLocaleDateString('en-US', { weekday: 'long' });
+
+  // get today's hours
+  const todayHours = shelterHours.find((day) => day.day === today);
+
+  // display today and the hours in an div with an id of hours
+  return (
+      <div id="hours">
+          <h2>Today's Hours</h2>
+          <h3><p>{todayHours.day} {todayHours.open} - {todayHours.close}</p></h3>
+      </div>
+  )
+}
 
 export default HoraDelDia;
